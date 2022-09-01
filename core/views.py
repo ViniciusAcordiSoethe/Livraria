37f97a1dc1e1362a36fc6edb4119d9410d190277
Soethe <soethe42@gmail.com>
2022-09-01 14:41:21 -0300

    Começando Generic Views do restFramework

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework.serializers import ModelSerializer
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView 
 from rest_framework import status
 
 from .forms import CategoriaForm
