5bde778b79a07be0491aaa00d66773df7abace0e
Soethe <soethe42@gmail.com>
2022-09-13 16:05:38 -0300

    Fazendo o serializer 2

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer , CharField
+from rest_framework.serializers import ModelSerializer , CharField , SerializerMethodField
 
 from core.models import Categoria , Editora , Autor , Livro
 
@@ -25,7 +25,16 @@ class LivroSerializer(ModelSerializer):
 
 class LivroDetailSerializer(ModelSerializer):
     categoria = CharField(source='categoria.descricao')
+    editora = EditoraSerializer() # sem usar a profundidade 
+    autores = SerializerMethodField()
     class Meta:
         model = Livro
         fields = '__all__'
         depth= 1
+
+    def get_autores(self,obj):
+        nome_autores = []
+        autores = obj.autores.get_queryset()
+        for autor in autores:
+            nome_autores.append(autor.nome)
+        return nome_autores
