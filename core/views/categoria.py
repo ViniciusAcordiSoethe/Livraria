63144a82a5146289d99900736ae9e2935fd1b63d
Soethe <soethe42@gmail.com>
2022-10-05 15:19:57 -0300

    Começando a autentificação

--- core/views/categoria.py
+++ core/views/categoria.py
@@ -1,4 +1,5 @@
 from core.models import Categoria
+from rest_framework.permissions import IsAuthenticated
 from core.serializers import CategoriaSerializer
 
 from rest_framework.viewsets import ModelViewSet
@@ -6,5 +7,6 @@ from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
 class CategoriaViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
